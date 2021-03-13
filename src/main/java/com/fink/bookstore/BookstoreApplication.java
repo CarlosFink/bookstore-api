8e62a2c9c258b9a10f6f4505a9a83dd211aa113e
CarlosFink <carlosfink@gmail.com>
2021-03-13 20:29:02 -0300

    instancia de forma automatica

--- src/main/java/com/fink/bookstore/BookstoreApplication.java
+++ src/main/java/com/fink/bookstore/BookstoreApplication.java
@@ -1,13 +1,38 @@
 package com.fink.bookstore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class BookstoreApplication {
+import com.fink.bookstore.domain.Categoria;
+import com.fink.bookstore.domain.Livro;
+import com.fink.bookstore.repositories.CategoriaRepository;
+import com.fink.bookstore.repositories.LivroRepository;
 
+@SpringBootApplication
+public class BookstoreApplication implements CommandLineRunner  {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private LivroRepository livroRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+		this.categoriaRepository.save(cat1);
+		
+		Livro livro1 = new Livro(null, "Clean Code", "Robert Martin", "Lorem Ipsum", cat1);		
+		cat1.getLivros().addAll(Arrays.asList(livro1));
+		this.livroRepository.save(livro1);			
 	}
 
 }
