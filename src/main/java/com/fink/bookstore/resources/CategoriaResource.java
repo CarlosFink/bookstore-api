2e83dd28ac6a8612a2af985a7e7d3c5380835bcc
CarlosFink <carlosfink@gmail.com>
2021-03-15 20:40:02 -0300

    POST para Categoria

--- src/main/java/com/fink/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/fink/bookstore/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.fink.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fink.bookstore.domain.Categoria;
 import com.fink.bookstore.dtos.CategoriaDTO;
@@ -17,20 +21,32 @@ import com.fink.bookstore.service.CategoriaService;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService categoriaService;
-	
+
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
 		Categoria categoria = categoriaService.findById(id);
 		return ResponseEntity.ok().body(categoria);
 	}
-	
+
 	@GetMapping
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = categoriaService.findAll();
-		List<CategoriaDTO> listDTO = list.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDTO);		
+		List<CategoriaDTO> listDTO = list.stream().map(categoria -> new CategoriaDTO(categoria))
+				.collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria categoria) {
+		categoria = categoriaService.create(categoria);
+		URI uri = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(categoria.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
